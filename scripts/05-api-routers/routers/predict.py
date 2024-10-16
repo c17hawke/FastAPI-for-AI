8d7e82b2e2f03f2ff0428d403fa6f155a93d9125
SUNNY BHAVEEN CHANDRA <sunny.c17hawke@gmail.com>
2024-10-16 20:44:04 +0530

    updates as per demo

--- scripts/05-api-routers/routers/predict.py
+++ scripts/05-api-routers/routers/predict.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-from typing import Any
 from fastapi import APIRouter, HTTPException, status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
@@ -7,32 +5,27 @@ import joblib
 import numpy as np
 from .config import Config
 
+router = APIRouter(tags=["Predict Route"])
+
 class PredictionInput(BaseModel):
     data: list[tuple[float, float, float, float]]
 
-model = None
-
-def load_model():
-    global model
-    if model is None:
-        model = joblib.load(Config.model_filepath)
-        return model
-    return model
-
-router = APIRouter(tags=["Predict"])
-
-@router.post("/predict/")
-async def predict(data: PredictionInput) -> dict[str, Any]:
-    model = load_model()
-    if model is None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Model not loaded")   
+@router.post("/predict")
+async def predict(data: PredictionInput) -> JSONResponse:
     try:
-        # Convert input data to NumPy array for prediction
+        model = joblib.load(Config.model_filepath)
+        # Make predictions using the loaded model
         input_data = np.array(data.data)
-        
-        # Make predictions
+
         predictions = model.predict(input_data)
-        
-        return JSONResponse(content={"predictions": predictions.tolist()})
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST , detail=str(e))
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={
+                "predictions": predictions.tolist()
+            }
+        )
+    except Exception as exe:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(exe)
+        )
--- scripts/05-api-routers/routers/train.py
+++ scripts/05-api-routers/routers/train.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from fastapi import APIRouter, UploadFile, File, HTTPException, status
 from fastapi.responses import JSONResponse
 import pandas as pd
@@ -11,38 +10,36 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from .config import Config
 
-router = APIRouter(tags=["train"])
+router = APIRouter(tags=["Train Route"])
 
-
-@router.post("/train/")
-async def train_model(file: UploadFile = File(...)) -> dict[str, str]:
-    global model
+@router.post("/train")
+async def train_model(file: UploadFile = File(...)) -> JSONResponse:
     try:
-        # Load CSV file into DataFrame
         df = pd.read_csv(file.file)
+
         # Assuming the last column is the target variable
         X = df.iloc[:, :-1]
         y = df.iloc[:, -1]
-        
+
         # Split data into training and testing sets
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        
+
         # Train a Random Forest Classifier
         model = RandomForestClassifier()
         model.fit(X_train.values, y_train.values)
-        
+
         # Make predictions and calculate accuracy
         predictions = model.predict(X_test)
 
         # Calculate accuracy score
         acc_score = accuracy_score(y_test, predictions)
-        
+
         # Calculate F1 score
         f1 = f1_score(y_test, predictions, average='weighted')
-        
+
         # Generate confusion matrix
         cm = confusion_matrix(y_test, predictions)
-        
+
         # Save confusion matrix as an image
         plt.figure(figsize=(10, 7))
         sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
@@ -54,7 +51,16 @@ async def train_model(file: UploadFile = File(...)) -> dict[str, str]:
 
         # Save the trained model
         joblib.dump(model, Config.model_filepath)
-        
-        return JSONResponse(content={"message": f"Model trained successfully with acc_score: {acc_score}! and f1_score: {f1}"})
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+        print(f"Model trained successfully with acc_score: {acc_score}! and f1_score: {f1}")
+        return JSONResponse(
+            content={
+                "message": f"Model trained successfully with acc_score: {acc_score}! and f1_score: {f1}"
+            },
+            status_code=status.HTTP_200_OK
+        )
+    except Exception as exe:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(exe)
+        )
